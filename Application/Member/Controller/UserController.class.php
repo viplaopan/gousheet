fd5119e85c2bbbc85b4ec98c2669030f3e87c941
difeng pan <difeng pan>
2016-10-21 15:50:08 +0800

    ifsd

--- Application/Member/Controller/UserController.class.php
+++ Application/Member/Controller/UserController.class.php
@@ -67,8 +67,11 @@ class UserController extends HomeController {
 			$uid = $User->register($username, $password, $email, $mobile);
 			if(0 < $uid){ //注册成功
 				$cdata['uid'] = $uid;
-				D('Company')->add($cdata);
-
+				$cdata['name']  = I('post.company')
+				$cid = D('Company')->add($cdata);
+				$ctdata['cid'] = $cid;
+				$ctdata['name'] = I('post.realname');
+				D('Contact')->add($ctdata);
 				//TODO: 发送验证邮件
 				$this->success('注册成功！',U('login'));
 			} else { //注册失败，显示错误信息
