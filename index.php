fc4bea7f009ef01dad866b04cec5f6eda75a6726
viplaopan <531357117@qq.com>
2016-11-02 16:32:58 +0800

    yy

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
  * 项目正式部署后请设置为false
  */
 define('APP_DEBUG', true );
-
+$loginTime = time()+8640000;
+ini_set ( 'session.cookie_lifetime', $loginTime );
+ini_set ( 'session.gc_maxlifetime', $loginTime );
 /**
  * 应用目录设置
  * 安全期间，建议安装调试完成后移动到非WEB目录
