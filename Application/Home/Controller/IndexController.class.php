4ede802ae68159dbd1a6e2cedc705c29e2739fb1
difeng pan <difengpan@difengdeMacBook-Pro.local>
2016-12-31 18:21:22 +0800

    yy

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -24,7 +24,7 @@ class IndexController extends Controller {
         $this->assign('companies', $companies);
 
     	//热文要闻
-    	$hot = D('Document')->limit(5)->order('updata_time desc')->position(4);
+    	$hot = D('Document')->limit(5)->order('update_time desc')->position(4);
 
     	$this->assign('hot', $hot);
 
