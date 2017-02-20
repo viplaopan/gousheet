eb705df96ad1b7794ae60dc6564e6661a9eb5613
Ringo <ringobool@outlook.com>
2017-02-20 13:32:20 +0800

    dsfds

---
+++ Application/Home/Controller/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ringo
+ * Date: 2017/2/20
+ * Time: 下午1:29
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+
+class HomeController extends Controller {
+{
+
+}
